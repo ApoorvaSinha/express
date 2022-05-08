62950d0edfb88535c0ed4fdc1e36631741384012
rohitabraham02 <rohit.abraham02@gmail.com>
2022-05-08 10:16:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // You can see your app's env variables in Kudu: https://<your app>.scm.azurewebsites.net/
 
-app.get('/', (req, res) => res.send('Hello World!version1'));
+app.get('/', (req, res) => res.send('Hello World!version2'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
